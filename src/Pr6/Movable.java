4a836d9e6c7e3edb3a9639355212a757a581ac94
666Dallas666 <s.mamonov.2002@yandex.ru>
2020-09-28 10:03:59 +0300

    init

---
+++ src/Pr6/Movable.java
@@ -0,0 +1,8 @@
+package Pr6;
+
+public interface Movable {
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+}
---
+++ src/Pr6/Rectangle.java
@@ -0,0 +1,57 @@
+package Pr6;
+
+public class Rectangle extends Shape {
+    protected double width;
+    protected double length;
+
+    @Override
+    public double getArea() {
+        return (width*length);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return (2*(width+length));
+    }
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double length) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public Rectangle(double width, double length, String color, boolean filled) {
+        this.length = length;
+        this.width = width;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", length=" + length +
+                "color='" + color + '\'' +
+                "filled=" + filled +
+                '}';
+    }
+}
